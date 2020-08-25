af01718be0eaca6aaa07d6992f1e75c99ccb9cb2
harshavardhan <harshavardan2k17@gmail.com>
2020-08-25 17:44:22 +0530

    checkboxes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { TreeList, Editing, Column, RequiredRule, Lookup, Button,ColumnChooser } from 'devextreme-react/tree-list';
+import React, {useState} from 'react';
+import { TreeList,ColumnChooser,Selection,ColumnVisibility } from 'devextreme-react/tree-list';
 import { parsedData } from './data3.js';
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  this.state = {
+    lineItemDescription: '',
+    rowSeq: ''
+  };
+  this.onFocusedRowChanged = this.onFocusedRowChanged.bind(this);
+  this.onTaskIdChanged = this.onTaskIdChanged.bind(this);
+}
+onTaskIdChanged(e) {
+  if(e.event && e.value > 0) {
+    this.setState({ focusedRowKey: e.value });
+  }
+}
+onFocusedRowChanged(e) {
+  var rowData = e.row && e.row.data;
+
+  if(rowData) {
+    this.setState({
+      lineItemDescription: rowData.lineItemDescription,
+      rowSeq: e.row.data.rowSeq
+    });
+  }
+  
+}
+
+
   render() {
+    // const [defaultHiddenColumnNames] = useState(['tag', 'cTag']);
     return (
       <div id="tree-list-parsed-data">
         <TreeList
@@ -14,10 +42,25 @@ class App extends React.Component {
           showBorders={true}
           columnAutoWidth={true}          
           keyExpr="rowSeq"
-          parentIdExpr="parentRowId"                   
+          parentIdExpr="parentRowId" 
+          focusedRowEnabled={true}
+          onFocusedRowChanged={this.onFocusedRowChanged}
         >    
-         <ColumnChooser enabled={true} mode="select" />      
+        {/* <ColumnVisibility
+          defaultHiddenColumnNames={defaultHiddenColumnNames}
+        /> */}
+         <ColumnChooser enabled={true} mode="select" /> 
+         <Selection  mode="multiple" />     
         </TreeList>
+
+        <div className="tree-list-info">
+
+          <div className="info">
+            <div id="lineItemDescription">{this.state.lineItemDescription}</div>
+            <div id="rowSeq">{this.state.rowSeq}</div>
+          </div>
+
+        </div>
       </div>
     );
   }
