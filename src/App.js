8527bea7a8a36c3a55edb066f398f56c9539671a
harshavardhan <harshavardan2k17@gmail.com>
2020-08-26 13:48:01 +0530

    column hiding

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React, {useState} from 'react';
-import { TreeList,ColumnChooser,Selection,ColumnVisibility } from 'devextreme-react/tree-list';
-import { parsedData } from './data3.js';
+import React, { useState } from "react";
+import { TreeList, ColumnChooser, Selection } from "devextreme-react/tree-list";
+import { parsedData } from "./data3.js";
+import { defaultHiddenColumnNames } from "./columnHiding.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-  this.state = {
-    lineItemDescription: '',
-    rowSeq: ''
-  };
-  this.onFocusedRowChanged = this.onFocusedRowChanged.bind(this);
-  //this.onTaskIdChanged = this.onTaskIdChanged.bind(this);
-}
-// onTaskIdChanged(e) {
-//   if(e.event && e.value > 0) {
-//     this.setState({ focusedRowKey: e.value });
-//   }
-// }
-onFocusedRowChanged(e) {
-  var rowData = e.row && e.row.data;
-
-  if(rowData) {
-    this.setState({
-      lineItemDescription: rowData.lineItemDescription,
-      rowSeq: e.row.data.rowSeq
-    });
+    this.state = {
+      lineItemDescription: "",
+      rowSeq: "",
+    };
+    this.onFocusedRowChanged = this.onFocusedRowChanged.bind(this);
   }
-  
-}
+  checkBoxAttributes = {
+    id: "rowSeq",
+  };
+  customizeColumns = (columns) => {
+    defaultHiddenColumnNames.forEach((colNameToHide) => {
+      columns.forEach((col) => {
+        if (col.dataField === colNameToHide) {
+          col.visible = false;
+        }
+      });
+    });
+  };
 
+  onFocusedRowChanged(e) {
+    var rowData = e.row && e.row.data;
+
+    if (rowData) {
+      this.setState({
+        lineItemDescription: rowData.lineItemDescription,
+        rowSeq: e.row.data.rowSeq,
+      });
+    }
+  }
 
   render() {
-    // const [defaultHiddenColumnNames] = useState(['tag', 'cTag']);
     return (
       <div id="tree-list-parsed-data">
         <TreeList
@@ -40,26 +45,22 @@ onFocusedRowChanged(e) {
           dataSource={parsedData}
           showRowLines={true}
           showBorders={true}
-          columnAutoWidth={true}          
+          columnAutoWidth={true}
           keyExpr="rowSeq"
-          parentIdExpr="parentRowId" 
+          parentIdExpr="parentRowId"
           focusedRowEnabled={true}
           onFocusedRowChanged={this.onFocusedRowChanged}
-        >    
-        {/* <ColumnVisibility
-          defaultHiddenColumnNames={defaultHiddenColumnNames}
-        /> */}
-         <ColumnChooser enabled={true} mode="select" /> 
-         <Selection  mode="multiple" />     
+          customizeColumns={this.customizeColumns}
+        >
+          <ColumnChooser enabled={true} mode="select" />
+          <Selection mode="multiple" />
         </TreeList>
 
         <div className="tree-list-info">
-
           <div className="info">
             <div id="lineItemDescription">{this.state.lineItemDescription}</div>
             <div id="rowSeq">{this.state.rowSeq}</div>
           </div>
-
         </div>
       </div>
     );
---
+++ src/columnHiding.js
@@ -0,0 +1,15 @@
+export const defaultHiddenColumnNames = [
+  "parentRowId",
+  "rowItemId",
+  "cTag",
+  "quantum",
+  "jsonTableId",
+  "dimension",
+  "currencyName",
+  "colItemIds",
+  "colItemIds",
+  "2019Q3_parsedDataId",
+  "2018Q3_parsedDataId",
+  "2019M9_parsedDataId",
+  "2018M9_parsedDataId",
+];
